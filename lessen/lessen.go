271aa8b8ab2ab08fb5a21231b4bed9115bd554c9
noom <johnuinchester@gmail.com>
2021-08-25 01:01:12 +0900

    sava my lessen

--- lessen/lessen.go
+++ lessen/lessen.go
@@ -13,11 +13,11 @@ const (
 )
 
 // initは初めに処理される特別な関数名
-func init()  {
+func init() {
 	fmt.Println("------ init ------")
 }
 
-func test()  {
+func test() {
 	fmt.Println("test")
 }
 
@@ -37,21 +37,21 @@ func circleArea(pi float64) func(radius float64) float64 {
 
 func hoge(params ...int) {
 	fmt.Println("length:", len(params), "value:", params)
-	for _, param := range params{
+	for _, param := range params {
 		fmt.Println(param)
 	}
 }
 
-func varLessen(){
+func varLessen() {
 
 	// ()でまとめて宣言可能
 	var (
-		i int = 1
-	 	f64 float64 = 1.2
-	 	s string = "test"
-	 	t,f        bool = true, false
-		iDefault   int
-		f64Default        float64
+		i                  int     = 1
+		f64                float64 = 1.2
+		s                  string  = "test"
+		t, f               bool    = true, false
+		iDefault           int
+		f64Default         float64
 		sDefault           string
 		tDefault, fDefault bool
 	)
@@ -70,7 +70,7 @@ func varLessen(){
 	fmt.Println(Pi, Name)
 }
 
-func add(x, y int) (int, int){
+func add(x, y int) (int, int) {
 	return x + y, x - y
 }
 
@@ -78,31 +78,113 @@ func cal(price, item int) (result int) {
 	result = price * item
 	return
 }
+
 //main関数がなければ動作しない
 func main() {
 	test()
-	fmt.Println("Hello world","TEST") //カンマ区切りで文字列連結
+	fmt.Println("Hello world", "TEST") //カンマ区切りで文字列連結
 	fmt.Println(time.Now())
 	fmt.Println(user.Current())
 
 	varLessen()
 
-	r1, r2 := add(10,20)
+	r1, r2 := add(10, 20)
 	fmt.Println(r1, r2)
 
 	r3 := cal(100, 200)
 	fmt.Println(r3)
 
 	// array
+	var a [2]int
+	a[0] = 100
+	a[1] = 200
+	fmt.Println("a:", a)
 
+	/* サイズが決まっているので下記のような記述はできない
+	var b [2]int = [2]int{100, 200}
+	b = append(b, 300)
+	*/
+
+	// slice
+	n := []int{1, 2, 3, 4, 5, 6}
+	fmt.Println("n:", n)
+	fmt.Println("n:", n[2])
+	fmt.Println("n:", n[2:4])
+	fmt.Println("n:", n[:2])
+	fmt.Println("n:", n[2:])
+	fmt.Println("n:", n[:])
+	n[2] = 100
+	fmt.Println("n:", n)
+
+	var board = [][]int{
+		[]int{0, 1, 2},
+		[]int{3, 4, 5},
+		[]int{6, 7, 8},
+	}
+	fmt.Println("board:", board)
+	n = append(n, 100)
+	fmt.Println("n:", n)
+	n = append(n, 100, 200, 300, 400)
+	fmt.Println("n:", n)
+
+	// slice make capacity
+	m := make([]int, 3, 5)
+	fmt.Printf("len=%d cap=%d value=%v\n", len(m), cap(m), m)
+	m = append(m, 0, 0)
+	fmt.Printf("len=%d cap=%d value=%v\n", len(m), cap(m), m)
+	m = append(m, 0, 0, 0, 0, 0)
+	fmt.Printf("len=%d cap=%d value=%v\n", len(m), cap(m), m)
+
+	l := make([]int, 3)
+	fmt.Printf("len=%d cap=%d value=%v\n", len(l), cap(l), l)
+
+	k := make([]int, 0) // メモリ確保あり
+	var o []int         // メモリ確保なし
+	fmt.Printf("len=%d cap=%d value=%v\n", len(k), cap(k), k)
+	fmt.Printf("len=%d cap=%d value=%v\n", len(o), cap(o), o)
+
+	o = make([]int, 5)
+	//o = make([]int, 0, 5)
+	for i := 0; i < 5; i++ {
+		o = append(o, i)
+		fmt.Println(o)
+	}
+	fmt.Println(o)
+
+	// map
+	p := map[string]int{"apple": 100, "banana": 200}
+	fmt.Println(p)
+	fmt.Println(p["apple"])
+	p["banana"] = 300
+	fmt.Println(p)
+	p["new"] = 500
+	fmt.Println(p)
+
+	fmt.Println(p["nothing"])
+
+	v, ok := p["apple"]
+	fmt.Println(v, ok)
+
+	v2, ok2 := p["nothing"]
+	fmt.Println(v2, ok2)
+
+	p2 := make(map[string]int)
+	p2["pc"] = 5000
+	fmt.Println(p2)
+
+	/*
+		var p3 map[string]int
+		p3["pc"] = 5000
+		fmt.Println(p3)
+	*/
 
 	// inner function
-	f := func(x int){
+	f := func(x int) {
 		fmt.Println("inner func", x)
 	}
 	f(2)
 
-	func (x int){
+	func(x int) {
 		fmt.Println("inner func", x)
 	}(2)
 
@@ -128,5 +210,3 @@ func main() {
 	// exercise
 	exercise1()
 }
-
-
