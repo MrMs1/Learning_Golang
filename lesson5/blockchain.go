f6cb2316707e2d10ba549252bb1415ea3d1abdcc
gitgub masatomomurata <johnuinchester@gmail.com>
2021-10-04 23:42:40 +0900

    save my lesson5 learning blockchain

---
+++ lesson5/blockchain.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+)
+
+type Block struct {
+	nonce        int
+	previousHash string
+	timestamp    int64
+	transactions []string
+}
+
+func NewBlock(nonce int, previousHash string) *Block {
+	b := new(Block)
+	b.timestamp = time.Now().UnixNano()
+	b.nonce = nonce
+	b.previousHash = previousHash
+	return b
+}
+
+func init() {
+	log.SetPrefix("Blockchain: ")
+}
+
+func main() {
+	log.Println("test")
+	fmt.Println("test2")
+}
