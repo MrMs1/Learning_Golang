af5ac4787b3c38b1360414f87dfeb8a600bfae5f
gitgub masatomomurata <johnuinchester@gmail.com>
2021-10-17 23:38:37 +0900

    save my lesson5

--- lesson5/blockchainserver/main.go
+++ lesson5/blockchainserver/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"flag"
 	"log"
 )
 
@@ -9,5 +9,8 @@ func init() {
 	log.SetPrefix("Blockchain: ")
 }
 func main() {
-	fmt.Println("test")
+	port := flag.Uint("port", 5000, "TCP Port Number for Blockchain Server")
+	flag.Parse()
+	app := NewBlockchainServer(uint16(*port))
+	app.Run()
 }
